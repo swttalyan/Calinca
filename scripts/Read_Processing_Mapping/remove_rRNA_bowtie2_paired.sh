4f130719d9a1dd94fdbb27678021f8eec45cc44e
swttalyan <swttalyan@gmail.com>
2020-03-23 00:02:10 +0100

    Update remove_rRNA_bowtie2_paired.sh

--- scripts/Read_Processing_Mapping/remove_rRNA_bowtie2_paired.sh
+++ scripts/Read_Processing_Mapping/remove_rRNA_bowtie2_paired.sh
@@ -2,7 +2,7 @@
 #export READ1=$1
 #export READ2=$2
 #export INDEX=$3
-module load bowtie
+module load bowtie2
 #bowtie2 -x /home/sweta/Desktop/LncRNAIdentificationSoftware/Scripts/GitCode/data/bowtie2rRNAindex/contaminants -S /dev/null --un-conc-gz rrna_free_reads/SRR1703431no_rRNA --al-conc-gz rrna_free_reads/SRR1703431_rRNA  -1 trimmed_reads/SN7640204_13427_cS_221223_dep_flexbar_1.fastq.gz -2 trimmed_reads/SN7640204_13427_cS_221223_dep_flexbar_2.fastq.gz
 
 bowtie2 -x $3 -1 $1 -2 $2 -S /dev/null --un-conc-gz $4"no_rRNA" --al-conc-gz $4"_rRNA" >& $5
