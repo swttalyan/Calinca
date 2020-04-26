e551f7fccf5de1d15b7ff87f6945d7f1026e4340
swttalyan <swttalyan@gmail.com>
2020-04-26 18:46:05 +0200

    Delete phylofile.sh

--- scripts/Sequence_And_Synteny_Conservation/SeqBlast/phylofile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -n 1
-#SBATCH -N 1
-#SBATCH --mem=500G
-
-perl human_mouse_file_forcyntenator.pl >New_Human_MouseCyntenator.blast
-#perl perl.pl
-#perl human_mouse_file_forcyntenator.pl >human_mouse.blast
